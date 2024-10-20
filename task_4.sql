3636305fc18c6a69cacf2dee02f1ff09481f64d6
Sondos91 <sondossfwat@gmail.com>
2024-10-20 15:04:00 +0300

    task41

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,3 @@
--- Get full description of the 'books' table from the 'alx_book_store' database
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' 
-AND TABLE_NAME = 'books';
+INFORMATION_SCHEMA.COLUMNS SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books';
