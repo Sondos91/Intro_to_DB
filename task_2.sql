5a2a21c41397034608a57b0e30ca83cb3493759b
Sondos91 <sondossfwat@gmail.com>
2024-10-20 14:50:22 +0300

    task23

--- task_2.sql
+++ task_2.sql
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCEs Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Create the ORDER_DETAILS table
